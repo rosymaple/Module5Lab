75fb27fd29baa9b42beb361f0128a21bd802ed0b
Candace Vogel <156586504+rosymaple@users.noreply.github.com>
2026-02-18 23:36:48 +0000

    -c

---
+++ SuperPlayer.cs
@@ -0,0 +1,13 @@
+namespace Module5Lab
+{
+    internal class SuperPlayer : Player
+    {
+        // Additional properties or methods specific to SuperPlayer
+        // For example:
+        public void Fly()
+        {
+            Console.WriteLine("SuperPlayer is flying!");
+        }
+    }
+}
+
